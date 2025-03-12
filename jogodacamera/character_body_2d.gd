4995c0285d67ddaf978d415f9f3531b0b2541103
Italoliraa <143004026+Italoliraa@users.noreply.github.com>
2025-03-12 20:25:18 -0300

    Add files via upload

---
+++ jogodacamera/character_body_2d.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+@export var velocidade = 200.0
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	if Input.is_action_pressed("ui_right"):
+		input_vector.x += 1
+	if Input.is_action_pressed("ui_left"):
+		input_vector.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		input_vector.y += 1
+	if Input.is_action_pressed("ui_up"):
+		input_vector.y -= 1
+
+	input_vector = input_vector.normalized() * velocidade
+	velocity = input_vector
+	move_and_slide()
